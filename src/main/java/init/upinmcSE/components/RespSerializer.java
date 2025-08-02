efbe0f2e0566ffcd276637841286e92978a2b361
Duong Trung Thanh <thanhdt3019@gmail.com>
2025-08-02 15:20:55 +0700

    RESP Deserialization

--- src/main/java/init/upinmcSE/components/RespSerializer.java
+++ src/main/java/init/upinmcSE/components/RespSerializer.java
@@ -2,9 +2,84 @@ package init.upinmcSE.components;
 
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class RespSerializer {
-    public void printWorking(){
-        System.out.println("-------------------------------------------------------------------------------------------");
+
+    private int getParts(char[] dataArray, int i, String[] subArray) {
+        int j=0;
+        while (i < dataArray.length && j < subArray.length) {
+            if(dataArray[i] == '$'){
+                // Bulk String
+                // $<length>\r\n\<data>\r\n
+                i++;
+                String partLength = "";
+                while (i < dataArray.length && Character.isDigit(dataArray[i])) {
+                    partLength += dataArray[i];
+                    i++;
+                }
+                i+=2;
+                String part = "";
+                for (int k = 0; k < Integer.parseInt(partLength); k++) {
+                    part += dataArray[i];
+                }
+                i+=2;
+                subArray[j++] = part;
+            }
+        }
+        return i;
+
+    }
+
+    public List<String[]> deseralize(byte[] command){
+//        [
+//                ["set", "key", "value"],
+//                ["set", "key", "value"]
+//        ]
+        String data = new String(command, StandardCharsets.UTF_8);
+        char[] dataArray = data.toCharArray();
+
+        List<String[]> result = new ArrayList<>();
+        int i = 0;
+
+        while(i < dataArray.length){
+            char currentChar = dataArray[i];
+            if(currentChar == '*'){
+                //"*42\r\n#3set\r\n"
+                // array
+                String arrayLen = "";
+                i++;
+                while(i < dataArray.length && Character.isDigit(dataArray[i])){
+                    arrayLen += dataArray[i];
+                }
+                i+=2;
+                if(dataArray[i] == '*'){
+                    // *2
+                    // *3\r\n#3set\r\n#3key\r\n#5value
+                    // *3\r\n#3set\r\n#3key\r\n#5value
+                    for(int t=0; t< Integer.parseInt(arrayLen); t++){
+                        String nestedLen = "";
+                        i++;
+                        while (i < dataArray.length && Character.isDigit(dataArray[i])){
+                            nestedLen += dataArray[i];
+                        }
+                        i+=2;
+                        String[] subArray = new String[Integer.parseInt(nestedLen)];
+                        i = getParts(dataArray, i, subArray);
+                        result.add(subArray);
+                    }
+                }else{
+                    // *3\r\n#3set\r\n#3key\r\n#5value
+                    String[] subArray = new String[Integer.parseInt(arrayLen)];
+                    i = getParts(dataArray, i, subArray);
+                    result.add(subArray);
+                }
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/init/upinmcSE/components/TcpServer.java
+++ src/main/java/init/upinmcSE/components/TcpServer.java
@@ -4,14 +4,94 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+
 @Component
 @NoArgsConstructor
 public class TcpServer {
     @Autowired
     private RespSerializer respSerializer;
 
-    public void startServer(){
-        respSerializer.printWorking();
+    public void startServer() {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            int id = 0;
+            while (true) {
+                clientSocket = serverSocket.accept();
+                id++;
+                Socket finalClientSocket = clientSocket;
+
+                InputStream inputStream = finalClientSocket.getInputStream();
+                OutputStream outputStream = finalClientSocket.getOutputStream();
+
+                Client client = new Client(finalClientSocket, inputStream, outputStream, id);
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        handleClient(client);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+    public void handleClient(Client client) throws IOException {
+        while (client.socket.isConnected()) {
+            byte[] buffer = new byte[client.socket.getReceiveBufferSize()];
+            int bytesRead = client.inputStream.read(buffer);
+            if (bytesRead > 0) {
+                // bytes parsing into strings
+                List<String[]> commands = respSerializer.deseralize(buffer);
+                for (String[] command : commands) {
+                    handleCommand(command, client);
+                }
+            }
+        }
+
+//        Scanner sc = new Scanner(client.inputStream);
+//        while(sc.hasNextLine()){
+//            String nextLine = sc.nextLine();
+//            System.out.println(nextLine);
+//            if(nextLine.contains("PING")){
+//                outputStream.write("THANH\r\n".getBytes());
+//            }
+//            if(nextLine.contains("ECHO")){
+//                String respHeader = sc.nextLine();
+//                String respBody = sc.nextLine();
+//                String response = respHeader + "\r\n" + respBody + "\r\n";
+//                outputStream.write(response.getBytes());
+//            }
+//    }
+    }
+
+    private void handleCommand(String[] command, Client client) {
+        for (String s : command) {
+
+        }
     }
 }
 
